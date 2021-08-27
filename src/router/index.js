f82480b8e5e823277d12ea39f92348d26c4941d2
Carlos_Bejarano <hestiam04@gmail.com>
2021-08-26 22:39:54 -0500

    Adding Home title and button

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
@@ -8,16 +8,20 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+  }, 
+  {
+    path: '/Panel',
+    name: 'Panel',
+    component: () => import(/* webpackChunkName: "panel" */ '../views/Panel.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/Product/:id',
+    name: 'Product',
+    component: () => import(/* webpackChunkName: "product" */ '../views/Product.vue')
   }
+
+  
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,44 @@
 <template>
-  <hello-world />
+  <v-container>
+    <h1 id="Title" class="text-left .font-weight-thin">Inter-Boutique</h1>
+    <v-btn id="s-button" depressed color="black" dark @click="toPanel"> Revisar Stock </v-btn>
+  </v-container>
 </template>
 
+
+
+<style scoped>
+#Title {
+  position: absolute;
+  width: 612px;
+  height: 94px;
+  left: 77px;
+  top: 172px;
+
+  font-style: normal;
+  font-weight: normal;
+  font-size: 72px;
+  line-height: 74px;
+}
+
+#s-button {
+  font-size: 12px;
+  font-style: normal;
+  font-weight: normal;
+  text-align: center;
+  top: 294px;
+  right: 12px;
+}
+</style>
 <script>
-  import HelloWorld from '../components/HelloWorld'
+export default {
+  name: "Home",
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+  methods:{
+    toPanel(){
+      this.$router.push('/Panel')
+    }
   }
+
+};
 </script>
